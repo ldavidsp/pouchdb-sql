be84443435223cda94d9645e8aec80ed94ae6f8b
Luis Solorzano <luisnic20@gmail.com>
2021-08-30 14:31:17 -0600

    Clean white spaces

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const utils = require('pouchdb-utils');
 const sqltomango = require('sqltomango');
 
 exports.sql = utils.toPromise(function (query, callback) {
-	if (typeof query !== 'string') {
-		throw('query must be a string');
-	}
+  if (typeof query !== 'string') {
+    throw('query must be a string');
+  }
 
   const cq = sqltomango.parse(query);
   if (Object.keys(cq).length === 0 || !cq.selector) {
-		cq.selector = {};
-	}
+    cq.selector = {};
+  }
 
-	return this.find(cq, callback);
+  return this.find(cq, callback);
 });
 
 if (typeof window !== 'undefined' && window.PouchDB) {
-	window.PouchDB.plugin(exports);
+  window.PouchDB.plugin(exports);
 }
